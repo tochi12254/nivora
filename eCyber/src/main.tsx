eee4cf5c9a3e78a1e2da0376f0297dd403cb3da9
james7dev <james2024karl@gmail.com>
2025-06-11 10:41:41 +0200

    added auathorization

--- eCyber/src/main.tsx
+++ eCyber/src/main.tsx
@@ -43,7 +43,7 @@ createRoot(rootElement).render(
               <Toaster />
               <Sonner />
               <BrowserRouter>
-                <AuthProvider>
+               <AuthProvider>
                   <App/>
                 </AuthProvider>
               </BrowserRouter>
